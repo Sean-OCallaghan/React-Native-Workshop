fc389be587c9a463a165462ccf10c27f9c544e10
Sean <seanoinca@gmail.com>
2021-02-13 12:38:11 -0800

    First

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDo from "./screens/ToDo"
+import Welcome from "./screens/Welcome"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+export default function App(){
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Welcome" component={Welcome}/>
+                <Tab.Screen name="Tasks" component={ToDo}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ yurt.js
@@ -0,0 +1,17 @@
+// import { StatusBar } from 'expo-status-bar';
+// import 'react-native-gesture-handler';
+// import { NavigationContainer } from '@react-navigation/native';
+// import React, {useState} from 'react';
+// import {Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import Task from "./components/toDo's";
+
+
+// export default function App() {
+
+
+
+//   return (
+   
+//   );
+// }
+
